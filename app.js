f69ddca00af8e7085f2dcad0ab00cf41f8dd7d69
Anderson Silva <anderson.araujo.08@gmail.com>
2018-01-18 21:45:29 -0300

    Mudanças no server.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
+var morgan = require("morgan");
 
 const express = require("express");
 const app = express();
@@ -9,11 +10,13 @@ var db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "login"
+  database: "flash"
 });
 
 app.set("views", "views/");
 app.set("view engine", "pug");
+
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -22,17 +25,37 @@ app.get("/", function(req, res) {
   res.sendFile(path.resolve("public/login.html"));
 });
 
-app.post("/login.html", (req, res) => {
+app.post("/cadastro", function(req, res) {
+  const { email, senha, nome, data_nascimento } = req.body;
+  db.query(
+    "insert into user values(?, ?, ?, ?)",
+    [email, senha, nome, data_nascimento],
+    (error, result, field) => {
+      if (error) {
+        res.send("Erro ao cadastrar, tente novamente.");
+      } else {
+        res.send("Cadastrado com sucesso.");
+      }
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
   const { email, senha } = req.body;
 
   db.query(
-    `select * from user where nome = "${email}" and password = "${senha}"`,
+    "select * from user where email = ? and senha = ?",
+    [email, senha],
     function(error, results, fields) {
-      if (error) throw error;
-      console.log("The solution is: ", results);
+      if (error) {
+        res.send("Erro ao realizar consulta.");
+      }
+
+      if (results.length != 0) {
+        res.redirect("/dashboard.html");
+      } else res.send("Erro ao fazer login. Tente novamente.");
     }
   );
-  res.send("Ola");
 });
 
 app.get("/dashboard.html", function(req, res) {
